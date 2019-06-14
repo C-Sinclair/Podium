158e7188a0484bf60e6a64b81ebe3fe524a76a25
Conor <conor@wilxite.com>
2019-06-14 14:27:28 +0100

    order mixer tracks by position

---
+++ packages/workstation/src/components/Mixer.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+type TrackInfo = {
+  name: String
+  position: number
+}
+
+type MixerProps = {
+  tracks: [TrackInfo]
+}
+
+const compare = (a: TrackInfo, b: TrackInfo) => {
+  let comp = 1
+  if (a.position < b.position) comp = -1
+  return comp
+}
+
+const Mixer: React.FunctionComponent<MixerProps> = props => {
+  const { tracks } = props
+  tracks.sort(compare)
+
+  return (
+    <ul>
+      {tracks.map((track: TrackInfo) => (
+        <li>{track.name}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default Mixer
